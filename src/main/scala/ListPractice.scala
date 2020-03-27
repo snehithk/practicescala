439c6f9647c21bf9c07be1b1873941588e0e3f35
snehithk <ksnehith@live.com>
2020-03-27 03:13:36 -0400

    Added code for  //Find the last but one element of a list.

--- src/main/scala/ListPractice.scala
+++ src/main/scala/ListPractice.scala
@@ -3,4 +3,8 @@ val li = List(1,2,3,4,5,6,7,8)
  println(li)
 
  println("last element=" +li.last)
+
+ //Find the last but one element of a list.
+ val li1 = li.init
+ println("last but one element=" +li1.last)
 }
